aea472ff1a2ce57266d5900e8c492360296fe9bf
A952 <sbandhyopadhyay@in.tesco.com>
2016-12-22 18:46:36 +0530

    solve review comments

--- fizz-buzz/src/main/java/com/fizz/buzz/FizzBuzz.java
+++ fizz-buzz/src/main/java/com/fizz/buzz/FizzBuzz.java
@@ -1,22 +1,22 @@
 package com.fizz.buzz;
+import static com.fizz.buzz.PrintOption.BUZZ;
+import static com.fizz.buzz.PrintOption.FIZZBUZZ;
+import static com.fizz.buzz.PrintOption.FIZZ;
 
 public class FizzBuzz {
 
-    public final static String FIZZ = "Fizz";
-    public final static String BUZZ = "Buzz";
-    public final static String FIZZBUZZ = "FizzBuzz";
 
-    public Object process(Integer input) {
+    public String verifyFizzBuzz(Integer input) {
         if (isDivisible(3, input) && isDivisible(5, input)) {
-            return FIZZBUZZ;
+            return FIZZBUZZ.value();
         }
         if (isDivisible(5, input)) {
-            return BUZZ;
+            return BUZZ.value();
         }
         if (isDivisible(3, input)) {
-            return FIZZ;
+            return FIZZ.value();
         }
-        return input;
+        return input.toString();
     }
 
     private boolean isDivisible(Integer divisible, Integer input) {
