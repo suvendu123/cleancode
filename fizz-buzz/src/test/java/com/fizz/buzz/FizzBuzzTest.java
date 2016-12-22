d760f8e2d26a311441087f8d4bc5969217e123aa
A952 <sbandhyopadhyay@in.tesco.com>
2016-12-22 12:00:54 +0530

    Fizz test

--- fizz-buzz/src/test/java/com/fizz/buzz/FizzBuzzTest.java
+++ fizz-buzz/src/test/java/com/fizz/buzz/FizzBuzzTest.java
@@ -1,12 +1,15 @@
 package com.fizz.buzz;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class FizzBuzzTest {
     
     @Test
     public void shouldPrintFizz(){
-        
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("Fizz", fizzBuzz.process(6));
     }
     
     @Test
