2e47d513b8ad474f6203223175dd71723158e10b
A952 <sbandhyopadhyay@in.tesco.com>
2016-12-22 12:04:56 +0530

    Buzz test

--- fizz-buzz/src/main/java/com/fizz/buzz/FizzBuzz.java
+++ fizz-buzz/src/main/java/com/fizz/buzz/FizzBuzz.java
@@ -3,7 +3,9 @@ package com.fizz.buzz;
 public class FizzBuzz {
 
     public Object process(int i) {
-        
+        if(i%5==0){
+            return "Buzz";
+        }
         return "Fizz";
     }
 
