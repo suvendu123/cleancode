d78db994b8d2bc6eb274d73d9371f19f8f92c5ae
A952 <sbandhyopadhyay@in.tesco.com>
2016-12-22 12:24:54 +0530

    Some test class code refactoring

--- fizz-buzz/src/main/java/com/fizz/buzz/FizzBuzz.java
+++ fizz-buzz/src/main/java/com/fizz/buzz/FizzBuzz.java
@@ -2,17 +2,21 @@ package com.fizz.buzz;
 
 public class FizzBuzz {
 
+    public final static String FIZZ = "Fizz";
+    public final static String BUZZ = "Buzz";
+    public final static String FIZZBUZZ = "FizzBuzz";
+
     public Object process(Integer i) {
-        if((i%3==0) && (i%5==0)){
-            return "FizzBuzz";
+        if ((i % 3 == 0) && (i % 5 == 0)) {
+            return FIZZBUZZ;
         }
-        if(i%5==0){
-            return "Buzz";
+        if (i % 5 == 0) {
+            return BUZZ;
         }
-        if(i%3==0){
-            return "Fizz";
+        if (i % 3 == 0) {
+            return FIZZ;
         }
-        
+
         return i;
     }
 
