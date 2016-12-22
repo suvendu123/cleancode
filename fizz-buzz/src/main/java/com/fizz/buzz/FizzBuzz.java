c769273c4532a44627fdeee1c9891b7791cf913f
A952 <sbandhyopadhyay@in.tesco.com>
2016-12-22 12:32:43 +0530

    method refactor.

--- fizz-buzz/src/main/java/com/fizz/buzz/FizzBuzz.java
+++ fizz-buzz/src/main/java/com/fizz/buzz/FizzBuzz.java
@@ -6,18 +6,22 @@ public class FizzBuzz {
     public final static String BUZZ = "Buzz";
     public final static String FIZZBUZZ = "FizzBuzz";
 
-    public Object process(Integer i) {
-        if ((i % 3 == 0) && (i % 5 == 0)) {
+    public Object process(Integer input) {
+        if (isDivisible(3, input) && isDivisible(5, input)) {
             return FIZZBUZZ;
         }
-        if (i % 5 == 0) {
+        if (isDivisible(5, input)) {
             return BUZZ;
         }
-        if (i % 3 == 0) {
+        if (isDivisible(3, input)) {
             return FIZZ;
         }
+        return input;
+    }
 
-        return i;
+    private boolean isDivisible(Integer divisible, Integer input) {
+        return input % divisible == 0;
+
     }
 
 }
