aa435b6cd822c1f19e8273258c3e50e1ac325da1
Suvendu <suvendu.banerjee80@gmail.com>
2016-12-26 20:35:32 +0530

    GREEN : Split the given sentence and  and simple put in a Map , to make the test pass

--- word-count/src/main/java/com/cleancode/wordcount/WordCount.java
+++ word-count/src/main/java/com/cleancode/wordcount/WordCount.java
@@ -1,12 +1,21 @@
 package com.cleancode.wordcount;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordCount {
 
-	public Map<String, Integer> count(String string) {
-		// TODO Auto-generated method stub
-		return null;
+	public Map<String, Integer> count(String sentence) {
+		String[] words = sentence.split(" ");
+		return getWordsMap(words);
+	}
+
+	private Map<String, Integer> getWordsMap(String[] words) {
+		Map<String, Integer> wordMap = new HashMap<String, Integer>();
+		for (String word : words) {
+			wordMap.put(word, 1);
+		}
+		return wordMap;
 	}
 
 }
--- word-count/src/test/java/com/cleancode/wordcount/WordCountTest.java
+++ word-count/src/test/java/com/cleancode/wordcount/WordCountTest.java
@@ -8,13 +8,13 @@ import java.util.Map;
 import org.junit.Test;
 
 public class WordCountTest {
-	
+
 	@Test
-	public void should_return_two_qnique_word(){
+	public void should_return_two_qnique_word() {
 		WordCount wordCount = new WordCount();
-		
+
 		Map<String, Integer> wordMap = wordCount.count("Hello World");
-		
+
 		assertEquals(2, wordMap.size());
 		assertTrue(wordMap.containsKey("Hello"));
 		
