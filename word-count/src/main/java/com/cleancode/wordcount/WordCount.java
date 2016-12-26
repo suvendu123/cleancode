e3873adba9cc0d506ac6d893bbbd405a78c9e6e1
Suvendu <suvendu.banerjee80@gmail.com>
2016-12-26 20:18:11 +0530

    RED : add a simple Hello world test case , which should return a Map contains "Hello" and "World" only once

---
+++ word-count/src/main/java/com/cleancode/wordcount/WordCount.java
@@ -0,0 +1,12 @@
+package com.cleancode.wordcount;
+
+import java.util.Map;
+
+public class WordCount {
+
+	public Map<String, Integer> count(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ word-count/src/test/java/com/cleancode/wordcount/WordCountTest.java
@@ -0,0 +1,23 @@
+package com.cleancode.wordcount;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Map;
+
+import org.junit.Test;
+
+public class WordCountTest {
+	
+	@Test
+	public void should_return_two_qnique_word(){
+		WordCount wordCount = new WordCount();
+		
+		Map<String, Integer> wordMap = wordCount.count("Hello World");
+		
+		assertEquals(2, wordMap.size());
+		assertTrue(wordMap.containsKey("Hello"));
+		
+	}
+
+}
