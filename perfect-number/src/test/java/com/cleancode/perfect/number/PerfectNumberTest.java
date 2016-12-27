8ed6428ef33997ecce4205bd83313da426ef540c
A952 <sbandhyopadhyay@in.tesco.com>
2016-12-27 18:30:30 +0530

    RED : second test case verify the given number is not perfect

--- perfect-number/src/test/java/com/cleancode/perfect/number/PerfectNumberTest.java
+++ perfect-number/src/test/java/com/cleancode/perfect/number/PerfectNumberTest.java
@@ -1,15 +1,28 @@
 package com.cleancode.perfect.number;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class PerfectNumberTest {
-    
+
+    private PerfectNumber number;
+
+    @Before
+    public void setUp() {
+        number = new PerfectNumber();
+    }
+
     @Test
-    public void should_return_true_if_perfect_number(){
-        PerfectNumber number = new PerfectNumber();
+    public void should_return_true_if_perfect_number() {
         assertTrue(number.isPerfect(6));
+    }
+
+    @Test
+    public void should_return_false_if_not_a_perfect_number() {
+        assertFalse(number.isPerfect(7));
     }
 
 }
