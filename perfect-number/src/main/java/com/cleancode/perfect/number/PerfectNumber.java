6b9400f9df34847bdf1788b24edf0dc83a5504c7
Suvendu <suvendu.banerjee80@gmail.com>
2016-12-27 21:01:12 +0530

    GREEN : Logic for find perfect number.

--- perfect-number/src/main/java/com/cleancode/perfect/number/PerfectNumber.java
+++ perfect-number/src/main/java/com/cleancode/perfect/number/PerfectNumber.java
@@ -2,8 +2,19 @@ package com.cleancode.perfect.number;
 
 public class PerfectNumber {
 
-    public boolean isPerfect(int inputNumber) {
-        return true;
-    }
+	public boolean isPerfect(int inputNumber) {
+		return getSumOfFactors(inputNumber) == inputNumber;
+
+	}
+
+	private int getSumOfFactors(int inputNumber) {
+		int sum = 0;
+		for (int i = 1; i <= inputNumber / 2; i++) {
+			if (inputNumber % i == 0) {
+				sum += i;
+			}
+		}
+		return sum;
+	}
 
 }
